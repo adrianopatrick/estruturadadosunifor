26897f10550523614a868f45f6958fb83f8ceb76
Adriano Patric Cunha <patrick.cunha@dataprev.gov.br>
2017-02-14 15:20:46 -0300

    incluído métodos genéricos

--- estruturadados/src/main/java/br/unifor/aulas/estruturadados/BuscaBinaria.java
+++ estruturadados/src/main/java/br/unifor/aulas/estruturadados/BuscaBinaria.java
@@ -1,9 +1,13 @@
 package br.unifor.aulas.estruturadados;
 
+import java.util.Comparator;
+
 public class BuscaBinaria {
 
+	/**
+	 * Complexidade: O(lg n)
+	 * */
 	public static Integer busca(Integer[] numbers, Integer valor) {
-		
 		if(valor != null) {
 			Integer inicio = 0;
 			Integer fim = numbers.length - 1; 
@@ -22,4 +26,26 @@ public class BuscaBinaria {
 		}
 		return null;
 	}
+	
+	
+	public static Integer buscar(Object[] objs, Object valor, Comparator<Object> comparator) {
+		if(valor != null) {
+			Integer inicio = 0;
+			Integer fim = objs.length - 1; 
+	
+			Integer media = (inicio + fim) / 2; 
+			while (inicio <= fim) {
+				if (objs[media].equals(valor)) {
+					return media;
+				} else if (comparator.compare(objs[media], valor) > 0) {
+					fim = media - 1; 
+				} else if (comparator.compare(objs[media], valor) < 0) { 
+					inicio = media + 1;  
+				}
+				media = (inicio + fim) / 2;	
+			}
+		}
+		return null;
+	}
+	
 }
--- estruturadados/src/test/java/br/unifor/aulas/estruturadados/BuscaBinariaTest.java
+++ estruturadados/src/test/java/br/unifor/aulas/estruturadados/BuscaBinariaTest.java
@@ -26,5 +26,27 @@ public class BuscaBinariaTest {
 		Integer valor = 52;
 		assertEquals(null, BuscaBinaria.busca(numbers, valor));
 	} 
+	
+	@Test
+	public void testBuscarNenhumValor() {
+		Integer numbers[] = { 10, 25, 36, 72, 121, 437, 590 };
+		Integer valor = null;
+		assertEquals(null, BuscaBinaria.buscar(numbers, valor, (o1, o2) -> ((Integer) o1).compareTo((Integer) o2)));
+	}
+
+	@Test
+	public void testBuscarValorEspecifico() {
+		Integer numbers[] = { 10, 25, 36, 72, 121, 437, 590 };
+		Integer valor = 72;
+		assertEquals((Integer) 3,
+				BuscaBinaria.buscar(numbers, valor, (o1, o2) -> ((Integer) o1).compareTo((Integer) o2)));
+	}
+
+	@Test
+	public void testBuscarValorInexistente() {
+		Integer numbers[] = { 10, 25, 36, 72, 121, 437, 590 };
+		Integer valor = 52;
+		assertEquals(null, BuscaBinaria.buscar(numbers, valor, (o1, o2) -> ((Integer) o1).compareTo((Integer) o2)));
+	}
 
 }
